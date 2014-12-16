c1221c6cf00c5d71d7cc6553944e52dd2d91844f
tristanKi <tristanKi@163.com>
2014-12-16 16:00:14 +0800

    log4j support

--- gdyh/src/main/java/com/mvn/gdyh/action/LoginAction.java
+++ gdyh/src/main/java/com/mvn/gdyh/action/LoginAction.java
@@ -1,8 +1,11 @@
 package com.mvn.gdyh.action;
 
+import org.apache.log4j.Logger;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class LoginAction extends ActionSupport {
+	Logger log = Logger.getLogger(LoginAction.class);
 	
 	@Override
 	public String execute() throws Exception {
@@ -12,6 +15,8 @@ public class LoginAction extends ActionSupport {
 	
 	public String checkUser() {
 		String ret = "success";
+		
+		log.debug("===========checkUser===========");
 		
 		return ret;
 	}
