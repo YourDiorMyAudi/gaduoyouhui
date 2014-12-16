6272dce7cec62bf5a5f8383a278ddcaa7fe47b28
tristanKi <tristanKi@163.com>
2014-12-16 15:15:23 +0800

    new gdyh project

---
+++ gdyh/src/main/java/com/mvn/gdyh/action/LoginAction.java
@@ -0,0 +1,18 @@
+package com.mvn.gdyh.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class LoginAction extends ActionSupport {
+	
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return super.execute();
+	}
+	
+	public String checkUser() {
+		String ret = "success";
+		
+		return ret;
+	}
+}
